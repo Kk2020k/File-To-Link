72d3e5aee4717b8e376e6314dca393797ddb8137
Kk2020k <108136211+Kk2020k@users.noreply.github.com>
2023-10-10 06:03:44 +0530

    Update shortlink.py

--- shortlink.py
+++ shortlink.py
@@ -5,8 +5,8 @@ from asyncio import TimeoutError
 from shortzy import Shortzy
 from Adarsh.vars import Var
 
-SHORTENER_API = str(getenv('SHORTENER_API', 'e0867ce24e2238645541bf7651be2217b4cd5dd1'))
-SHORTENER_WEBSITE = str(getenv('SHORTENER_WEBSITE', 'shorturllink.in'))
+SHORTENER_API = str(getenv('SHORTENER_API', '62b1ec93449a6ac2f78d6d16e8cb76227951eac3'))
+SHORTENER_WEBSITE = str(getenv('SHORTENER_WEBSITE', 'tnshort.net'))
 
 
 shortzy = Shortzy(SHORTENER_API, SHORTENER_WEBSITE)
